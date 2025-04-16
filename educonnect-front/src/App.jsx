6f212352910513e67c07de8e5ddc65fe90f06be3
Angemirainda <angemirainda12@gmail.com>
2025-04-16 17:14:16 +0100

    new version

--- educonnect-front/src/App.jsx
+++ educonnect-front/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/homePage.jsx'; // ta page d'accueil
+import RegisterPage from './pages/registerPage.jsx';
+import LoginPage from './pages/loginPage.jsx';
 // import App from './App.jsx'
 import CandidaturePage from './pages/CandidaturePage.jsx'
  import './App.css'
@@ -17,15 +19,17 @@ import CandidaturePage from './pages/CandidaturePage.jsx'
   
 // )
 
-function App() {
+function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/candidature" element={<CandidaturePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
 }
 
-export default App;
+export default AppRoutes;
